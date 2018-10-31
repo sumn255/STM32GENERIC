0045e3c39c87830db8fad68820f9452df2769c5e
huaweiwx <huaweiwx@sina.com>
2018-10-31 22:44:01 +0800

    fixd f1 medium density portTickUse_TIMx to 0

--- STM32/variants/ATKMINI_F103R/configs/HAL_Conf.h
+++ STM32/variants/ATKMINI_F103R/configs/HAL_Conf.h
@@ -9,7 +9,11 @@
 /***************  for freeRtos ****************/
 
 #ifndef portTickUSE_TIMx
-    #define portTickUSE_TIMx 7   /* 0/7 use systick/TIM7*/
+# if defined(STM32F103RE)
+    #define portTickUSE_TIMx 7
+# else
+    #define portTickUSE_TIMx 0   /* 0/7 use systick/TIM7*/
+# endif
 #else
 # if (portTickUSE_TIMx !=0)||(portTickUSE_TIMx !=7)
 #   error "portTickUSE_TIMx must be 0 or 7"
--- STM32/variants/BLUEPILL/configs/HAL_Conf.h
+++ STM32/variants/BLUEPILL/configs/HAL_Conf.h
@@ -9,7 +9,7 @@
 /***************  for freeRtos ****************/
 
 #ifndef portTickUSE_TIMx
-    #define portTickUSE_TIMx 7   /* 0/7 use systick/TIM7*/
+    #define portTickUSE_TIMx 0   /* 0/7 use systick/TIM7*/
 #else
 # if (portTickUSE_TIMx !=0)||(portTickUSE_TIMx !=7)
 #   error "portTickUSE_TIMx must be 0 or 7"
